b1f61f7e4d49bd795b13da0b1d41ca7c561f13f6
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-07-24 12:31:27 +0600

    White space remover rule added

--- script.js
+++ script.js
@@ -88,12 +88,13 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
    */
   e.preventDefault();
   // Get form values
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const isbn = document.querySelector("#isbn").value;
+  const title = document.querySelector("#title").value.trim();
+  const author = document.querySelector("#author").value.trim();
+  const isbn = document.querySelector("#isbn").value.trim();
 
   // validate
 
+  console.log(title);
   /**
    *
    *
@@ -102,7 +103,7 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
    *
    *
    */
-  if (!title || !author || !isbn  && title === ' ' || author === ' ' || isbn === ' ') {
+  if (!title || !author || !isbn) {
     UI.showAlert("please fill all fields", "danger");
   } else {
     // Instantiate book
